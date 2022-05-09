d1cce8100be2c7714dce2221539856c9bc6541de
Snehita Kolli <snehitakolli20@gmail.com>
2022-05-08 20:34:02 -0700

    api folder pf routes finish

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,61 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// To SIGN UP
+router.post('/', async (req, res) => {
+    try{
+        const newUser = await User.create({
+            username: req.body.username,
+            password: req.body.password
+        });
+
+        req.session.save(() => {
+            req.session.userId = newUser.id;
+            res.session.username = newUser.username;
+            req.session.loggedIn = true;
+
+            res.json(newUser);
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }    
+});
+
+// To LOGIN 
+router.post('/login', async (req, res) => {
+    try{
+        const user = await User.findOne({
+            where: {
+                username: req.body.username,
+            }, 
+        });
+
+        if (!user) {
+            res.status(400).json({ message: 'No user account found.' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.userId = user.id;
+            req.session.username = user.username;
+            req.session.loggedIn = true; 
+
+            res.json({ user, message: 'You are now logged in!' });
+        });
+    } catch (err) {
+        res.status(400).json({ message: 'No user account found.' });
+    }
+});
+
+// To LOGOUT
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+  
+module.exports = router;
