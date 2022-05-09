d269375b91564d02c68eae9408adc6a370ce16ef
Snehita Kolli <snehitakolli20@gmail.com>
2022-05-08 22:51:38 -0700

    finish utils folder and initial css

---
+++ utils/helpers.js
@@ -0,0 +1,9 @@
+module.exports = {
+    format_time: (date) => {
+        return date.toLocaleTimeString();
+    },
+    format_date: (date) => {
+        // Using JavaScript Date methods, we get and format the month, date, and year
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+    },
+};
