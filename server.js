17a918dd298acd1ec25a5cebdf00d82d3bd790d1
Snehita Kolli <snehitakolli20@gmail.com>
2022-05-09 16:51:08 -0700

    changed server.js for server startup

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+app.listen(PORT, () => {
+    console.log(`App now listening on ${PORT}!`);
+    sequelize.sync({ force: false });
 });
